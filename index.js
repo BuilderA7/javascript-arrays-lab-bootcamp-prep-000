475e42304621f427c8f08f0de55c7538d3b6f3d7
A I <atbisrael@gmail.com>
2018-01-04 07:31:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function destructivelyRemoveFirstKitten() {
    return kittens;
 }
 function appendKitten(name) {
-  var newarray = [...kittens, window.name];
+  var newarray = [...kittens.slice(0, kittens.length - 1), window.name];
   return newarray
 }
 function prependKitten(name) {
