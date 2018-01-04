64f3f5b2fd880a4336ba0d42a927afc19e87c14d
A I <atbisrael@gmail.com>
2018-01-04 07:36:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function destructivelyRemoveFirstKitten() {
    return kittens;
 }
 function appendKitten(name) {
-  var newarray = [...kittens, window.name];
+  var newarray = [...kittens, name];
   return newarray
 }
 function prependKitten(name) {
-  window.kittens = [window.name,...kittens];
+  window.kittens = [name,...kittens];
   return kittens;
 }
 function removeLastKitten() {
