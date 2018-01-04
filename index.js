805a627c9d9ea7eeffe9026ba9b93742a6bd24b0
A I <atbisrael@gmail.com>
2018-01-04 07:26:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+   
+   window.kittens.push(name);
+  return kittens;
+}
+function destructivelyPrependKitten(name){
+  
+  window.kittens.unshift(name);
+  return kittens;
+}
+function destructivelyRemoveLastKitten() {
+   window.kittens.pop();
+  return kittens;
+}
+function destructivelyRemoveFirstKitten() {
+   window.kittens.shift();
+   return kittens;
+}
+function appendKitten(name) {
+  window.kittens = [...kittens, window.name];
+  return kittens
+}
+function prependKitten(name) {
+  window.kittens = [window.name,...kittens];
+  return kittens;
+}
+function removeLastKitten() {
+  return window.kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten() {
+  return window.kittens.slice(1);
+}
